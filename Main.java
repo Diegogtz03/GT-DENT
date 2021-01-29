66e7db672d9526bb612b9a06d28bdc394f75359c
Diego Gutiérrez <65473367+Diegogtz03@users.noreply.github.com>
2021-01-28 22:47:49 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,386 @@
+package database;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+
+import com.dropbox.core.DbxApiException;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.UploadErrorException;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import javax.swing.JOptionPane;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Main {
+	
+	static Connection connection;
+	private static final String ACCESS_TOKEN = "yVe6P-Jb0gYAAAAAAAAAAc-q_s6wjHWZXMfv5UFeBVROzcIBQ8FLhK-fN0o0erq3";
+	public static DbxClientV2 client;
+	private static String path = "/Users/diegogutierrez/Desktop/TEC 5/Info III/Imagenes Programa/";
+
+	/**
+	 * El metodo main() inicializa las conexiones necesarias para el programa como lo es la conexion
+	 * a la base de datos (Local) y la conexion a el Dropbox para el respaldo
+	 */
+	public static void main(String[] args) throws SQLException, DbxApiException, DbxException {
+		getConnection();
+	}
+
+	
+	public static void getConnection() {
+		String url = "jdbc:sqlserver://localhost\\Consul:1433;databaseName=Consul";
+		String username = "sa";
+		String password = "reallyStrongPwd123";
+
+		try  {
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(ProgramaMama.getFrmExpodent(), "No se pudo conectar a la base de datos"
+					+ "\nCheque su base de datos");
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		try {
+			getDropBoxConnection();
+		} catch (DbxException e) {
+			JOptionPane.showMessageDialog(ProgramaMama.getFrmExpodent(), "No se pudo conectar a Dropbox"
+					+ "\nCheque su conexion a internet");
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void getDropBoxConnection() throws DbxApiException, DbxException {
+		DbxRequestConfig config = DbxRequestConfig.newBuilder("GT DENT/1.0").build();
+		client = new DbxClientV2(config, ACCESS_TOKEN);
+	}
+	
+	/**
+	 * metodo que recibe parametros para introducir a la base de datos
+	 * y se introducen a la tabla de pacientes en la base de datos
+	 * se introduce un expediente y odontograma vacio para el paciente
+	 */
+	public static void createLine(String name, String fNacimiento, String edad, String correo, String tel) throws SQLException {
+		String sql = "INSERT INTO PacientesTEST (fechaNacimiento, correo, edad, Nombre, telefono)"
+				   + "VALUES ('" +  fNacimiento + "', '" + correo + "', '" + edad + "', '" + name + "', '" + tel +"')";
+
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql);
+		ExpedienteVacio();
+		insertBlankOdontograma();
+	}
+
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * de la tabla de los pacientes
+	 */
+	public static ResultSet getLines() throws SQLException {
+		String sql = "SELECT * FROM PacientesTEST";
+		Statement statement = connection.createStatement(); 
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * de la tabla de pacientes limitados por ese nombre
+	 */
+	public static ResultSet refresh(String name) throws SQLException {
+		String sql = "SELECT * FROM PacientesTEST WHERE Nombre LIKE '" + name + "%'";
+		Statement statement = connection.createStatement(); 
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * de la tabla de pacientres limitados por la fecha
+	 * de nacimiento introducida
+	 */
+	public static ResultSet refreshConFecha(String fechaNacimiento) throws SQLException {
+		String sql = "SELECT * FROM PacientesTEST WHERE fechaNacimiento LIKE '" + fechaNacimiento + "%'";
+		Statement statement = connection.createStatement(); 
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	/**
+	 * metodo que cambia los datos de cierto paciente cuando
+	 * se pide algun cambio de dato
+	 */
+	public static void updatePatient(String name, String fNacimiento, String edad, String correo, String tel, String i) throws SQLException {
+		String sql = "UPDATE PacientesTEST SET Nombre = '" + name + "', fechaNacimiento = '" + fNacimiento + "', edad = '" + edad + "', correo ='" + correo +  "', telefono ='" + tel +"' WHERE # = " + i;
+		Statement statement = connection.createStatement();
+		statement.execute(sql);
+	}
+	
+	/**
+	 * metodo que regresa el diagnostico del paciente
+	 * dependido en el id del paciente y el diagnostico
+	 * que se este desplegando
+	 */
+	public static String diagnostic(int Dnum, int CurPa) throws SQLException {
+		String temp = "Diagnostico" + Dnum;
+		String sql = "SELECT " + temp + " FROM Expedientes WHERE Id = '" + CurPa + "';";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);	
+		result.next();
+		return result.getString(temp);
+	}
+	
+	/**
+	 * metodo que regresa un string con el contenido
+	 * de la proxima cita dependiendo el id del paciente
+	 */
+	public static String Citas(int CurPa) throws SQLException {
+		String sql = "SELECT ProxCita FROM Expedientes WHERE Id = '" + CurPa + "';";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);	
+		result.next();
+		return result.getString("ProxCita");
+	}
+	
+	/**
+	 * metodo que regresa un string con el contenido
+	 * de los antecedentes de un paciente dependiendo
+	 * el id del paciente
+	 */
+	public static String Antecedentes(int CurPa) throws SQLException {
+		String sql = "SELECT Antecedentes FROM Expedientes WHERE Id = '" + CurPa + "';";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);	
+		result.next();
+		return result.getString("Antecedentes");
+	}
+	
+	/**
+	 * metodo que introduce un expediente vacio para que
+	 * los id's de los pacientes no queden mal enlazados
+	 */
+	private static void ExpedienteVacio() throws SQLException {
+		String sql = "INSERT INTO Expedientes (Diagnostico1, Diagnostico2, Diagnostico3, Diagnostico4, ProxCita, Antecedentes)"
+				+ "VALUES ('" +  "" + "', '" + "" + "', '" + "" + "', '" + "" + "', '" + "" + "', '" + "" + "')";
+		
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql);
+	}
+	
+	/**
+	 * metodo que guarda los datos de la proxima cita,
+	 * los antecedentes en la base de datos dependiendo
+	 * en el id del paciente seleccionado
+	 */
+	public static void saveCitaData(String prox, String Antece, int curPac) throws SQLException {
+		String sql = "UPDATE Expedientes SET ProxCita = '" + prox + "', Antecedentes = '" + Antece + "' WHERE Id = '" + curPac + "';";
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql);
+	}
+	
+	/**
+	 * metodo que guarda los datos de los diagnosticos
+	 * del paciente en la base de datos depediendo en 
+	 * el numero de diagnostico y el id de paciente
+	 */
+	public static void diagnosticInsert(String insertar, int diaNUM, int CurPac) throws SQLException{
+		String sql = "UPDATE Expedientes SET Diagnostico" + diaNUM + " = '" + insertar + "' WHERE Id = '" + CurPac + "';";
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql);
+	}
+	
+	/**
+	 * metodo que introduce una fila de odontograma
+	 * vacia para que las tablas no queden mal enlazados
+	 */
+	public static void insertBlankOdontograma() throws SQLException {
+		String sql = "INSERT INTO Odontograma DEFAULT VALUES";
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql);
+	}
+	
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * del odontograma con el id del paciente 
+	 */
+	public static ResultSet getOdontograma(int curPac) throws SQLException {
+		String sql = "SELECT * FROM Odontograma WHERE Id = '" + curPac + "';";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	/**
+	 * metodo que guarda el odontograma del paciente
+	 * donde toma el numero de odontograma y el id
+	 * de paciente como parametro de entrada
+	 */
+	public static void guardarOdontograma(String curNum, int curPac, String texto) throws SQLException {
+		String sql = "UPDATE Odontograma SET [" + curNum + "] = '" + texto + "' WHERE Id = '" + curPac + "';";
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(sql);
+	}
+	
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * de los pacientes en donde se revisa si ya hay
+	 * un paciente con un correo igual al dado
+	 */
+	public static ResultSet revisarCorreo(String correo) throws SQLException{
+		String sql = "SELECT * FROM PacientesTEST WHERE correo = '" + correo + "';";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * de los expedientes de todos los pacientes
+	 */
+	public static ResultSet getAllExpedientes() throws SQLException {
+		String sql = "SELECT * FROM Expedientes";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	/**
+	 * metodo que regresa un resultset con el contenido
+	 * de todos los odontogramas de todos los pacientes
+	 */
+	public static ResultSet getAllOdontogramas() throws SQLException {
+		String sql = "SELECT * FROM Odontograma";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(sql);
+		return result;
+	}
+	
+	
+	/**
+	 * metodo que realiza todos los pasos para realizar
+	 * el respaldo en el archivo de excel
+	 */
+	public static void respaldar() throws SQLException, IOException, URISyntaxException {
+		//se envia el archivo de excel al metodo anterior para llenarlo de datos
+		//junto con el resultset de los pacientes
+		Workbook excel = WorkbookFactory.create(true);
+		ResultSet pacientes = getLines();
+		excel = archivoExcel(pacientes, excel, "Pacientes");
+		
+		//se envia el mismo archivo de excel, junto con el resultset
+		//de los expedientes hacia el metodo anterior
+		ResultSet expedientes = getAllExpedientes();
+		excel = archivoExcel(expedientes, excel, "Expedientes");
+		
+		//se envia el mismo archivo de excel, junto con el resultset
+		//de los odontogramas hacia el metodo anterior
+		ResultSet odontogramas = getAllOdontogramas();
+		excel = archivoExcel(odontogramas, excel, "Odontogramas");
+		
+		//ya que estan todos los datos en el excel
+		//se genera un archivo temporal para enviarlo
+		//a dropbox
+		try(FileOutputStream archivo = new FileOutputStream(path + "prueba10.xlsx")){
+			excel.write(archivo);
+			archivo.flush();
+			archivo.close();
+
+			archivoDropBox(archivo);
+		} catch (UploadErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DbxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * metodo para generar el archivo de respaldo en excel de la base de datos
+	 * con el uso de la libreria Apache Poi utilizando un resultset, un archivo
+	 * de excel y un titulo para realizarlo con esos datos
+	 */
+	public static Workbook archivoExcel(ResultSet result, Workbook excel, String titulo) 
+			throws URISyntaxException, IOException, SQLException {
+
+		//se obtiene los datos del ResultSet para asi escribirlos en el excel
+		java.sql.ResultSetMetaData resultPacientes = result.getMetaData();
+		List<String> columnas = new ArrayList<String>() {{
+
+			//se itera por la el resultado de la base de datos para escribirlos
+			//en la lista de strings
+			for(int i = 1; i <= resultPacientes.getColumnCount(); i++) {
+				add(resultPacientes.getColumnLabel(i));
+			}
+		}};
+
+		//se genera una pagina en el archivo de excel para agregar la informacion
+		Sheet paginaPacientes = excel.createSheet(titulo);
+		Row tituloColumna = paginaPacientes.createRow(0);
+
+		//se itera por la columnas y se agrega el titulo de cada columna
+		for(int i = 0; i < columnas.size(); i++) {
+			tituloColumna.createCell(i).setCellValue(columnas.get(i));
+		}
+
+		int filaActual = 0;
+		//se itera por la columnas y se agregan los datos por cada nueva fila
+		while(result.next()) {
+			Row fila = paginaPacientes.createRow(++filaActual);
+			for(int i = 0; i < columnas.size(); i++) {
+				fila.createCell(i).setCellValue(Objects.toString(result.getObject(columnas.get(i)), ""));
+			}
+		}
+		return excel; 	
+	}
+	
+	/**
+	 * metodo para enviar el archivo de respaldo de excel a 
+	 * dropbox
+	 */
+	public static void archivoDropBox(FileOutputStream archivoFinal) 
+			throws UploadErrorException, DbxException, IOException {
+		//se genera una fecha para asi nombrar el archivo de ese día
+		Date fecha = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd:MM:yyyy");
+		String nombreArchivo = "Respaldo-GTDENT-" + formatter.format(fecha) + ".xlsx";
+
+		//se accede a dropbox quitando el archivo anterior y agregando el nuevo con la
+		//fecha nueva del día del respaldo
+		InputStream in;
+		try {
+			client.files().deleteV2("/GT DENT");
+			client.files().createFolderV2("/GT DENT");
+			in = new FileInputStream(path + "prueba10.xlsx");
+			client.files().uploadBuilder("/GT DENT/" + nombreArchivo).uploadAndFinish(in);
+			JOptionPane.showMessageDialog(ProgramaMama.getFrmExpodent(), "Se respaldo correctamente");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//se carga el archivo de nuevo para eliminarlo por completo de la computadora
+		File temp = new File(path + "prueba10.xlsx");
+		temp.delete();
+	}
+}
---
+++ Ventana_receta.java
@@ -0,0 +1,362 @@
+package database;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.Document;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument; 
+import org.apache.pdfbox.pdmodel.PDPage; 
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageable;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.printing.PDFPageable;
+import org.apache.pdfbox.printing.PDFPrintable;
+import org.apache.pdfbox.printing.Scaling;
+import org.apache.pdfbox.PDFReader;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.pdfviewer.PageWrapper;
+
+import com.mysql.cj.result.Field;
+import com.spire.pdf.PdfDocument;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Shape;
+
+import javax.imageio.ImageIO;
+import javax.print.PrintService;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.MediaPrintableArea;
+import javax.print.attribute.standard.MediaSize;
+import javax.print.attribute.standard.OrientationRequested;
+import javax.print.attribute.standard.PrinterResolution;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.print.Book;
+import java.awt.print.PageFormat;
+import java.awt.print.Pageable;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
+public class Ventana_receta extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField_nombre;
+	private JTextField textField_fecha;
+	private JTextArea textArea_contenido;
+	private static String path = "/Users/diegogutierrez/Desktop/TEC 5/Info III/Imagenes Programa/";
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//se genera un objeto de Ventana_receta
+					Ventana_receta frame = new Ventana_receta();
+					//se hace visible
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws IOException 
+	 */
+	
+	public Ventana_receta() throws IOException {
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1644, 947);
+		setLocation(dim.width/2 - getSize().width/2, dim.height/2- getSize().height/2);
+		/**
+		 * tomado de... 
+		 */
+		contentPane = new JPanel() {
+			public void paintComponent( Graphics g ){
+				  super.paintComponent(g);
+				  Graphics2D g2d = (Graphics2D) g;
+				  try {
+					InputStream image = new FileInputStream(path + "fondo blob.png");
+					  BufferedImage src;
+					  try {
+						src = ImageIO.read(image);
+						g2d.drawImage(src, 0, 0, null);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				}
+		};
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btn_regresar = new JButton("<");
+		btn_regresar.setForeground(new Color(255, 160, 122));
+		btn_regresar.setBackground(Color.WHITE);
+		btn_regresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				ProgramaMama.getFrmExpodent().setVisible(true);
+			}
+		});
+		btn_regresar.setFont(new Font("Lucida Grande", Font.BOLD, 24));
+		btn_regresar.setBounds(53, 43, 94, 86);
+		contentPane.add(btn_regresar);
+		
+		textField_nombre = new RoundJTextField(15);
+		textField_nombre.setBounds(510, 229, 241, 32);
+		contentPane.add(textField_nombre);
+		textField_nombre.setColumns(10);
+		Border borde = BorderFactory.createLineBorder(Color.black);
+		textField_nombre.setBorder(borde);
+		
+		textField_fecha = new RoundJTextField(15);
+		textField_fecha.setBounds(898, 195, 142, 32);
+		contentPane.add(textField_fecha);
+		textField_fecha.setColumns(10);
+		textField_fecha.setBorder(borde);
+		
+		JButton btnImprimir = new JButton("Imprimir");
+		btnImprimir.setBackground(SystemColor.textHighlight);
+		btnImprimir.setFont(new Font("Lucida Grande", Font.BOLD, 30));
+		btnImprimir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					cargarPdf();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnImprimir.setBackground(SystemColor.textHighlight);
+		btnImprimir.setBounds(1182, 340, 332, 174);
+		contentPane.add(btnImprimir);
+		
+		JButton btnVaciar = new JButton("VACIAR");
+		btnVaciar.setFont(new Font("Lucida Grande", Font.BOLD, 21));
+		btnVaciar.setBackground(Color.WHITE);
+		btnVaciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField_nombre.setText("");
+				textField_fecha.setText("");
+				textArea_contenido.setText("");
+			}
+		});
+		btnVaciar.setBounds(1261, 541, 184, 60);
+		contentPane.add(btnVaciar);	
+		
+		textArea_contenido = new JTextArea();
+		textArea_contenido.setBounds(457, 363, 528, 354);
+		contentPane.add(textArea_contenido);
+		textArea_contenido.setBorder(borde);
+		
+		JButton btnDaDeHoy = new JButton("día de hoy");
+		btnDaDeHoy.setBackground(Color.WHITE);
+		btnDaDeHoy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ponerFecha();
+			}
+		});
+		btnDaDeHoy.setBounds(1299, 613, 117, 29);
+		contentPane.add(btnDaDeHoy);
+	}
+	
+	/**
+	 * metodo para cargar el pdf de la receta y asi mismo
+	 * escribir el contenido en la receta con el uso de la
+	 * libreria "PdfBox"
+	 */
+	private void cargarPdf() throws IOException {
+		//guardar en variables los datos a poner en la receta
+		String nombre = textField_nombre.getText();
+		String fecha = textField_fecha.getText();
+		String contenido = textArea_contenido.getText();
+		
+		//se carga el archivo .pdf para asi agregar el contenido de las variables
+		File archivo = new File(path + "Receta.pdf");
+		PDDocument recetaArchivo = PDDocument.load(archivo);
+		PDPage recetaPagina = recetaArchivo.getPage(0);
+		PDPageContentStream escritura = new PDPageContentStream(recetaArchivo, recetaPagina, PDPageContentStream.AppendMode.APPEND, true, true);
+		
+		//se inicia el texto en el pdf en las cordenadas dadas
+		escritura.beginText();
+		escritura.newLineAtOffset(128, 607);
+		escritura.setFont(PDType1Font.TIMES_ROMAN, 12);
+		escritura.showText(nombre);
+		
+		//se inicia una nueva linea de texto en la nueva cordenada
+		escritura.newLine();
+		escritura.newLineAtOffset(338, 29);
+		escritura.showText(fecha);
+		escritura.newLine();
+		escritura.setFont(PDType1Font.TIMES_ROMAN, 12);
+		String[] textoDividido = contenido.split("\n");
+		int x = 0;
+		
+		//finalmente se escriben las lineas separadas por espacios
+		//en una nueva linea de texto en el pdf
+		for (int i = 0; i < textoDividido.length; i++) {
+			String texto = textoDividido[i];
+			if(i == 1) {
+				x-=13;
+				escritura.newLineAtOffset(0, x);
+			} else if (i > 1) {
+				x-=1;
+				escritura.newLineAtOffset(0, x);
+			} else if (i == 0) {
+				escritura.newLineAtOffset(-368, -139);
+			}
+			escritura.showText(texto);
+		}
+		
+		//se finalizan y se cierran los cuadros de texto para guardarlos en el archivo
+		escritura.endText();
+		escritura.close();
+		
+		//se guarda el archivo editado a un archivo temporal
+		recetaArchivo.save(path + "RecetaFINAL.pdf");
+
+		//se manda a imprimir el archivo
+		try {
+			printPDF();
+		} catch (PrinterException e) {
+			e.printStackTrace();
+		}
+
+		//una vez impreso, se cierra el archivo para no generar errores
+		recetaArchivo.close();
+	}
+		
+
+	/**
+	 * metodo para imprimir el archivo editado por el metodo cargarPdf()
+	 */
+	private static void printPDF() throws IOException, PrinterException {
+		//se carga el archivo editado por el metodo cargarPdf()
+		File f = new File(path + "RecetaFINAL.pdf");
+		f.setReadOnly();
+		PDDocument docFinal = PDDocument.load(f);
+		
+		//se genera un trabajo de impreseion y se pide la informacion al cliente
+	    PrinterJob job = PrinterJob.getPrinterJob();
+	    PageFormat formato = job.getPageFormat(null);
+	    Paper papel = formato.getPaper();
+	    
+	    //se posiciona el tamaño y configuracion de la pagina para que se logre una impresion completa
+	    papel.setSize(612, 792);
+	    papel.setImageableArea(0, 0, formato.getPaper().getWidth(), formato.getPaper().getHeight());
+	    formato.setOrientation(PageFormat.PORTRAIT);
+	    formato.setPaper(papel);
+	    Book pageable = new Book();
+	    pageable.append(new PDFPrintable(docFinal, Scaling.ACTUAL_SIZE, true, (float)500.0), formato, docFinal.getNumberOfPages());
+	    job.setPageable(pageable);
+	    job.printDialog();
+	    PrintService impresora = job.getPrintService(); 
+	    job.setPrintService(impresora);
+	    job.setJobName("Receta Dra.Sandra");
+	    job.setCopies(1);
+	    
+	    //se manda a imprimir la pagina
+	    job.print();
+	    
+	    //se cierra el documento para evirar errores y se elimina el archivo temporal 
+	    docFinal.close();
+	    f.delete();
+	}
+	
+	/**
+	 * metodo para modificar la forma de los JTextFields
+	 * tomado de: https://stackoverflow.com/a/8515677
+	 */
+	public class RoundJTextField extends JTextField {
+	    private Shape shape;
+	    public RoundJTextField(int size) {
+	        super(size);
+	        setOpaque(false);
+	    }
+	    protected void paintComponent(Graphics g) {
+	         g.setColor(getBackground());
+	         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+	         super.paintComponent(g);
+	    }
+	    protected void paintBorder(Graphics g) {
+	         g.setColor(getForeground());
+	         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+	    }
+	    public boolean contains(int x, int y) {
+	         if (shape == null || !shape.getBounds().equals(getBounds())) {
+	             shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+	         }
+	         return shape.contains(x, y);
+	    }
+	}
+	
+	/**
+	 * metodo para crea la fecha del dia de hoy
+	 * y pone la fecha en el TextField en donde va
+	 * la fecha cuando se presiona el boton
+	 */
+	private void ponerFecha() {
+		Date fecha = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		textField_fecha.setText(formatter.format(fecha));
+	}
+	
+	public JTextField getTextFieldNombre() {
+		return textField_nombre;
+	}
+	
+	public JTextField getTextFieldFecha() {
+		return textField_fecha;
+	}
+	
+}
